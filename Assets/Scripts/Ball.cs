c36cfcde3071e332845781c3038e08a7f2465401
Tommy Slaatbraaten <tommy.slaatbraaten@hotmail.com>
2023-12-26 19:11:43 +0100

    base

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,15 +6,17 @@ public class Ball : MonoBehaviour
 {
     List<Vector2> possibleDirection = new List<Vector2>()
     {
-        new Vector2(0, 1),  // Up
-        new Vector2(1, 1),  // Diagonal Right Up
-        new Vector2(-1, 1), // Diagonal Left Up
-        new Vector2(0, -1), // Down
-        new Vector2(1, -1), // Diagonal Right Down
-        new Vector2(-1, -1), // Diagonal left Down
+        new Vector2(0.2f, 1.2f),  // Up
+        new Vector2(0.2f, 1.2f),  // Diagonal Right Up
+        new Vector2(-1.2f, 1.2f), // Diagonal Left Up
+        new Vector2(0.2f, -1.2f), // Down
+        new Vector2(1.2f, -1.2f), // Diagonal Right Down
+        new Vector2(-1.2f, -1.2f), // Diagonal left Down
         
     };
-    [SerializeField] float speedIncreaseFactor = 1;
+    [SerializeField] float speedIncreaseFactor = 1.1f;
+    [SerializeField] float currentSpeed;
+    [SerializeField] float maxSpeed = 10f;
 
     Rigidbody2D rb;
     // Start is called before the first frame update
@@ -23,18 +25,21 @@ public class Ball : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         StartDirection();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void Update()
     {
-        if (collision.gameObject.CompareTag("Paddle"))
+        currentSpeed = rb.velocity.magnitude;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Paddle") || other.CompareTag("Wall"))
         {
-            IncreaseSpeed();
-            ReflectDirection(collision.contacts[0].normal);
-        }
-        else if (collision.gameObject.CompareTag("Wall"))
-        {
-            ReflectDirection(collision.contacts[0].normal);
+            Vector2 normal = CalculateReflectionNormal(other);
+            ReflectBall(normal);
+            // Handle any additional interactions, like speed increase
         }
     }
+
 
     private void StartDirection()
     {
@@ -51,11 +56,45 @@ public class Ball : MonoBehaviour
     private void IncreaseSpeed()
     {
         rb.velocity *= speedIncreaseFactor;
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
     }
-    private void ReflectDirection(Vector2 normal)
+    private void ReflectBall(Vector2 normal)
     {
         Vector2 incomingVector = rb.velocity;
         rb.velocity = Vector2.Reflect(incomingVector, normal);
+        IncreaseSpeed();
     }
+    private Vector2 CalculateReflectionNormal(Collider2D collider)
+    {
+        if (collider.CompareTag("Wall"))
+        {
+            // For vertical walls
+            if (transform.position.x > collider.transform.position.x)
+            {
+                // Ball is to the right of the wall, so the wall is likely on the left
+                return new Vector2(1, 0); // Normal pointing to the right
+            }
+            else
+            {
+                // Ball is to the left of the wall, so the wall is likely on the right
+                return new Vector2(-1, 0); // Normal pointing to the left
+            }
+        }
+        else if (collider.CompareTag("Paddle"))
+        {
+            // For horizontal paddles
+            if (transform.position.y > collider.transform.position.y)
+            {
+                // Ball is above the paddle, so the paddle is likely the bottom one
+                return new Vector2(0, 1); // Normal pointing upwards
+            }
+            else
+            {
+                // Ball is below the paddle, so the paddle is likely the top one
+                return new Vector2(0, -1); // Normal pointing downwards
+            }
+        }
 
+        return Vector2.zero; // Default case, should not usually happen
+    }
 }
